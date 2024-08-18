9c4c7ddaba5517820ab21eb99dd8f3a8faa88301
debian12 <bclubdotcc@gmail.com>
2024-08-18 16:41:47 +0800

    update .gitignore

--- internal/nginx/filter.go
+++ internal/nginx/filter.go
@@ -1,6 +1,7 @@
 package nginx
 
 import (
+	"log"
 	"regexp"
 )
 
@@ -16,21 +17,43 @@ func NewNginx(key string) *Nginx {
 
 	return &Nginx{
 		key: key,
-		reg: regexp.MustCompile(`((?:\d{1,3}\.){3}\d{1,3}).*\[(.*)\s[+-]\d{4}\]\s['"](\w*?)\s((` + key + `){1,})(\?(.*)){0,1}?\s(.*?)['"]\s(\d+)\s\d+\s['"].*?['"]\s['"](.*?)['"]`),
+		reg: regexp.MustCompile(`(?<ip>(?:\d{1,3}\.){3}\d{1,3}).*\[(?<time>.*\s[+-]\d{4})\]\s['"](?<method>\w*?)\s(?<path>(?:` + key + `){1,})(?<params>\?.*){0,1}?\s(?<protocol>.*?)['"]\s(?<status_code>\d+)\s\d+\s['"].*?['"]\s['"](?<user_agent>.*?)['"]`),
 	}
 }
 
 func (ng *Nginx) Filter(origin string) (string, string) {
 	if origin != "" {
 		subMatchResults := ng.reg.FindStringSubmatch(origin)
-
-		// for i, v := range subMatchResults {
-		// 	log.Println(i, v)
-		// }
-		// os.Exit(0)
-		if len(subMatchResults) == 11 {
-			return subMatchResults[1], subMatchResults[9] + " " + subMatchResults[3] + " " + subMatchResults[4]
+		groupNames := ng.reg.SubexpNames()
+		if len(subMatchResults) == 0 || len(groupNames) == 0 {
+			// log.Printf("未匹配到结果: %v\n", origin)
+			return "", ""
 		}
+
+		if len(subMatchResults) != len(groupNames) {
+			log.Fatalf("匹配异常: %v\n", origin)
+		}
+
+		var (
+			ip         string
+			statusCode string
+			path       string
+			method     string
+		)
+		for i, v := range groupNames {
+			switch v {
+			case "ip":
+				ip = subMatchResults[i]
+			case "status_code":
+				statusCode = subMatchResults[i]
+			case "path":
+				path = subMatchResults[i]
+			case "method":
+				method = subMatchResults[i]
+			}
+		}
+
+		return ip, statusCode + " " + method + " " + path
 	}
 
 	return "", ""
